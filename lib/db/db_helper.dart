371a2b55f419e2b24bd1016aedff3a74c916f17e
Shubham Gupta <shubhamgupta7117@gmail.com>
2022-05-31 00:05:40 +0530

    db helper class

---
+++ lib/db/db_helper.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+import '../model/agency.dart';
+
+class DBHelper {
+  static sql.Database? agencyDB;
+  static Future<void> createDB() async {
+    String dbFullPath = path.join(await sql.getDatabasesPath(), 'agency_db.db');
+    agencyDB =
+        await sql.openDatabase(dbFullPath, version: 1, onCreate: (db, version) {
+      return db.execute('Create table LPGAgency('
+          'aid integer primary key,'
+          'aname Text,'
+          'address Text,'
+          'lat double,'
+          'long double)');
+    });
+  }
+
+  static Future<void> insertAgencyData() async {
+    // final agency = Agency();
+    if (agencyDB != null) {
+      print('Hwe');
+      for (var item in Agency.agencies) {
+        await agencyDB!.insert(
+          'LPGAgency',
+          item.toMap(),
+        );
+      }
+    }
+  }
+
+  static Future<List<Agency>?> fetchAgencyList() async {
+    final List<Map<String, dynamic>> dbData;
+    if (agencyDB != null) {
+      dbData = await agencyDB!.query('LPGAgency');
+      print("Che" + dbData.toString());
+      return List.generate(
+        dbData.length,
+        (index) => Agency(
+          aid: dbData[index]['aid'],
+          aname: dbData[index]['aname'],
+          address: dbData[index]['address'],
+          lat: dbData[index]['lat'],
+          long: dbData[index]['long'],
+        ),
+      );
+    }
+    return null;
+  }
+}
